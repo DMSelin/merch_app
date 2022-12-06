4c41665e1dbac60b2a32f93fbbd21c8cb4241a46
Dmselin <dmselin.ws@gmail.com>
2022-12-06 15:15:31 +0300

    Создал файл приложения. Определил  размер окна

---
+++ app_merch.py
@@ -0,0 +1,21 @@
+import tkinter as tk 
+
+mainwindow = tk.Tk()
+mainwindow.title('merch app')
+mainwindow.geometry('500x600')
+mainwindow.resizable(width=False, height=False)
+
+frame_list = tk.Frame(mainwindow, width=200, height=300, bg='#E0FFFF')
+frame_list.grid(row=0, column=0)
+
+frame_choice = tk.Frame(mainwindow, width=300, height=300, bg='#FFEBCD')
+frame_choice.grid(row=0, column=1)
+
+frame_changes = tk.Frame(mainwindow, width=500, height=300, bg='#DCDCDC')
+frame_changes.grid(row=1, column=0, columnspan=2)
+
+l_routs = tk.Label(frame_list, text='Маршруты').pack()
+l_report = tk.Label(frame_list, text='Отчет').pack()
+l_positions = tk.Label(frame_list,text='Позиции').pack()
+
+mainwindow.mainloop()
