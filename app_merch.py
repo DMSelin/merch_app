f65ab9c13c9ab96f6afb331a2f14d1735021701d
Dmselin <dmselin.ws@gmail.com>
2022-12-13 10:37:36 +0300

    Создал файл с базой данных и наполнил ее данными

--- app_merch.py
+++ app_merch.py
@@ -5,8 +5,8 @@ mainwindow.title('merch app')
 mainwindow.geometry('500x600')
 mainwindow.resizable(width=False, height=False)
 
-frame_list = tk.Frame(mainwindow, width=200, height=300, bg='#E0FFFF')
-frame_list.grid(row=0, column=0)
+frame_menu = tk.Frame(mainwindow, width=200, height=300, bg='#E0FFFF')
+frame_menu.grid(row=0, column=0)
 
 frame_choice = tk.Frame(mainwindow, width=300, height=300, bg='#FFEBCD')
 frame_choice.grid(row=0, column=1)
@@ -14,8 +14,8 @@ frame_choice.grid(row=0, column=1)
 frame_changes = tk.Frame(mainwindow, width=500, height=300, bg='#DCDCDC')
 frame_changes.grid(row=1, column=0, columnspan=2)
 
-l_routs = tk.Label(frame_list, text='Маршруты').pack()
-l_report = tk.Label(frame_list, text='Отчет').pack()
-l_positions = tk.Label(frame_list,text='Позиции').pack()
+l_routs = tk.Label(frame_menu, text='Маршруты').pack(expand=1)
+l_report = tk.Label(frame_menu, text='Отчет').pack(expand=1)
+l_positions = tk.Label(frame_menu,text='Позиции').pack()
 
 mainwindow.mainloop()
