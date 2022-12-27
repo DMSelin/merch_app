f8a152c5faf74a38aaad1d1b6b8793ff84b89f57
Dmselin <dmselin.ws@gmail.com>
2022-12-27 12:27:59 +0300

    Add function: Reading last emails from mail.ru.

--- mail.py
+++ mail.py
@@ -3,8 +3,8 @@ import email
 from email.header import decode_header
 import base64
 #from bs4 import BeautifulSoup
-import re
 from configparser import ConfigParser
+import asyncio
 
 urlsconf ='config/config.ini'
 config =ConfigParser() 
@@ -18,13 +18,26 @@ print(imap.login(username, mail_pass))
 
 # imap.list()
 # folders = imap.list()
-# print(folders)
 # for folder in folders[1]:
 #     print(folder)
+def read_mails(count_mails):
+    last_mails = list()
+    for i in range(count_mails-15, count_mails):
+        res, msg = imap.fetch(f'{i}'.encode(), '(RFC822)')  #Для метода search по порядковому номеру письма
+        msg = email.message_from_bytes(msg[0][1])
+    
+        letter_subject = msg["Subject"]
+        last_mails.append(decode_header(letter_subject)[0][0].decode())
+    return last_mails
+# Подклбчение к папке Отправленные
+sent_emails = (imap.select(b"&BB4EQgQ,BEAEMAQyBDsENQQ9BD0ESwQ1-"))
+count_mails = int(sent_emails[1][0].decode("utf-8"))
+received_mail = read_mails(count_mails)
+for i in received_mail:
+    print(i)
 
-print(imap.select(b"&BB4EQgQ,BEAEMAQyBDsENQQ9BD0ESwQ1-"))
-
-# ddate = '12-December-2022'
-# result, data = imap.uid('search', None, '(SENTON %s)' % ddate)
-# print(data)
-#imap.uid()
+# letter_date = email.utils.parsedate_tz(msg["Date"]) # дата получения, приходит в виде строки, дальше надо её парсить в формат datetime
+# letter_id = msg["Message-ID"] #айди письма
+# letter_from = msg["Return-path"] # e-mail отправителя
+# print(letter_date)
+# print(type(letter_date), type(letter_id), letter_id, type(letter_from))
