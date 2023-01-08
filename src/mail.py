134d3aae9f8ab5b99cb458ba20880c8617a9c47b
Dmselin <dmselin.ws@gmail.com>
2023-01-08 17:35:06 +0300

    put the functions in a separate file

--- src/mail.py
+++ src/mail.py
@@ -1,73 +1,16 @@
-import imaplib
-import email
-import base64
-from email.header import decode_header 
-from configparser import ConfigParser
+import sys
+import modules_of_mail
 
-urlsconf ='src/config/config.ini'
-config =ConfigParser() 
-config.read(urlsconf) 
-
-mail_pass = config['login_mail_ru']['mail_pass'] 
-username = config['login_mail_ru']['username']
-imap_server = "imap.mail.ru"
-imap = imaplib.IMAP4_SSL(imap_server)
-print(imap.login(username, mail_pass))
-
-# imap.list()
-# folders = imap.list()
-# for folder in folders[1]:
-#     print(folder)
-def read_mails(count_mails):
-    last_mails = list()
-    text_mails = list()
-    for i in range(count_mails, count_mails-5, -1):
-        res, msg = imap.fetch(f'{i}'.encode(), '(RFC822)')  
-        msg = email.message_from_bytes(msg[0][1]) # Извлекаем часть с содержанием
-        letter_subject = msg["Subject"] # Извлекаем тему сообщения в кодеровке
-        subject = decode_header(letter_subject)[0][0].decode() # Декодируем в текст
-        first_word_subject = subject[:subject.find(" ")] # Вырезаем первое слово из содержания
-        if first_word_subject == "Отчет": # При совпадении перовго слова со словом "Отчет", выходим из цикла
-            break
-        last_mails.append(subject) # Добавляю запись в список
-
-        for part in msg.walk():
-            if part.get_content_maintype() == 'text' and part.get_content_subtype() == 'plain':
-                text = base64.b64decode(part.get_payload()).decode()
-                text_mails.append(text)   
-
-    return last_mails, text_mails  # Возвращаю список писем за сегодня и текст писем
+imap = modules_of_mail.connection()
+if not imap:
+    sys.exit()
 
 # Подключение к папке "Отправленные"
 sent_emails = (imap.select(b"&BB4EQgQ,BEAEMAQyBDsENQQ9BD0ESwQ1-"))
 count_mails = int(sent_emails[1][0].decode("utf-8"))
-print(count_mails)
-received_mails_subjects, recieved_mails_texts = read_mails(count_mails) 
+received_mails_subjects, recieved_mails_body = modules_of_mail.read_mails(count_mails, imap) 
 
-# Создаю список названий магазинов из полученного списка названий отчетов
-name_stores = list()
-for i in received_mails_subjects:
-    if i.find(",", 0, 11) == -1:
-        name_store = i[:i.find(" ")]
-    else:
-        name_store = i[:i.find(",")]
-    name_stores.append(name_store)
-    
-# Создаю список названий адресов из полученного списка названий отчетов
-name_streets = list()
-for i in received_mails_subjects:
-    name_street = i[i.find(" "):i.rfind(",")]
-    name_streets.append(name_street)
+name_stores = modules_of_mail.extract_store_names(received_mails_subjects)
+name_streets = modules_of_mail.extract_street_names(received_mails_subjects)
+message_body = modules_of_mail.extract_message_body(recieved_mails_body)
 
-# Создаю список комментариев из полученного списка текстов писем
-comment_mails = list()
-for i in recieved_mails_texts:
-    i = i.replace("С уважением Селин Денис", "")
-    i = i.replace("\n", "")
-    comment_mails.append(f"{i}\n")
-
-# letter_date = email.utils.parsedate_tz(msg["Date"]) # дата получения, приходит в виде строки, дальше надо её парсить в формат datetime
-# letter_id = msg["Message-ID"] #айди письма
-# letter_from = msg["Return-path"] # e-mail отправителя
-# print(letter_date)
-# print(type(letter_date), type(letter_id), letter_id, type(letter_from))
---
+++ src/modules_of_mail.py
@@ -0,0 +1,71 @@
+import imaplib
+import email
+import base64
+from configparser import ConfigParser
+from email.header import decode_header 
+
+def connection():
+    urlsconf ='src/config/config.ini'
+    config =ConfigParser() 
+    config.read(urlsconf) 
+
+    mail_pass = config['login_mail_ru']['mail_pass'] 
+    username = config['login_mail_ru']['username']
+    imap_server = config['imap_server']['imap_server']
+    imap = imaplib.IMAP4_SSL(imap_server)
+    sts, res = imap.login(username, mail_pass)
+    if sts == "OK":
+        return imap
+    else:
+        return False
+
+def read_mails(count_mails, imap):
+    last_mails = list()
+    text_mails = list()
+    for i in range(count_mails, count_mails-5, -1):
+        res, msg = imap.fetch(f'{i}'.encode(), '(RFC822)')  
+        msg = email.message_from_bytes(msg[0][1]) # Извлекаем часть с содержанием
+        letter_subject = msg["Subject"] # Извлекаем тему сообщения в кодеровке
+        subject = decode_header(letter_subject)[0][0].decode() # Декодируем в текст
+        first_word_subject = subject[:subject.find(" ")] # Вырезаем первое слово из содержания
+        if first_word_subject == "Отчет": # При совпадении перовго слова со словом "Отчет", выходим из цикла
+            break
+        last_mails.append(subject) # Добавляю запись в список
+
+        for part in msg.walk():
+            if part.get_content_maintype() == 'text' and part.get_content_subtype() == 'plain':
+                text = base64.b64decode(part.get_payload()).decode()
+                text_mails.append(text)   
+
+    return last_mails, text_mails  # Возвращаю список писем за сегодня и текст писем
+
+def extract_store_names(subject):
+    # Создаю список названий магазинов из полученного списка названий отчетов
+    name_stores = list()
+    for i in subject:
+        if i.find(",", 0, 11) == -1:
+            name_store = i[:i.find(" ")]
+        else:
+            name_store = i[:i.find(",")]
+        name_stores.append(name_store)
+    
+    return name_stores
+
+def extract_street_names(subject):     
+    # Создаю список названий адресов из полученного списка названий отчетов
+    name_streets = list()
+    for i in subject:
+        name_street = i[i.find(" "):i.rfind(",")]
+        name_streets.append(name_street)
+
+    return name_streets   
+
+def extract_message_body(text_body):
+    # Создаю список комментариев из полученного списка текстов писем
+    comment_mails = list()
+    for i in text_body:
+        i = i.replace("С уважением Селин Денис", "")
+        i = i.replace("\n", "")
+        comment_mails.append(f"{i}\n")
+
+    return comment_mails
