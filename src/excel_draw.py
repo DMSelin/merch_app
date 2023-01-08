b80a06ffd787fd0b95a5a3e2f53380577c5d4e43
Dmselin <dmselin.ws@gmail.com>
2023-01-08 17:41:17 +0300

    Add excel drawing file

---
+++ src/excel_draw.py
@@ -0,0 +1,32 @@
+import openpyxl as opx
+import pandas as pd
+from openpyxl import load_workbook, Workbook
+from datetime import date, datetime
+
+import stores_for_report as sfr
+import formatting as frmt
+
+excel_file = Workbook()
+excel_file.create_sheet("Отчет", 0)
+active_sheet = excel_file.active
+
+report_sheet = frmt.FormattingTable(active_sheet)
+
+for data in sfr.pytrchka():
+    active_sheet.append(data)
+
+for data in sfr.perek():
+    active_sheet.append(data)
+
+report_sheet.column_alignment_center(['A', 'E', 'F', 'G'])
+report_sheet.column_alignment_left(['B', 'C', 'D', 'H', 'I'])
+
+report_sheet.set_border(active_sheet)
+report_sheet.set_font(active_sheet)
+
+excel_file.save('report.xlsx')
+
+# Конвертирую Страницу в DataFrame
+df = pd.DataFrame(active_sheet.values)
+
+print(df)
